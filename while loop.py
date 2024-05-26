e30d65842a289a69fb7caac440a44a5375790859
arnell80 <arnell80@gmail.com>
2024-05-25 18:16:00 -0700

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,17 @@
+# while loop. A statment that will execute its block of code as long as it's condition remains true
+
+
+name = ""
+
+while len(name) == 0:
+        name = input("Enter your name: ")
+
+print("Hello "+name)
+
+
+name1 = None
+
+while not name1:
+    name1 = input("Enter Your Name: ")
+
+print("Hello "+name1)
